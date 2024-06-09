6f57303e7fa9a5ea7710c1ec51f4f8a7ef2cac84
haletaleila <leilahaleta@gmail.com>
2024-06-09 17:50:28 +0900

    java with gitignore

---
+++ ch1-1/src/App.java
@@ -0,0 +1,26 @@
+class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        if (a == b) {
+            System.out.println(true);
+        }
+    }
+}
+/**
+ * 925858445
+ * 925858445
+ * true
+ */
